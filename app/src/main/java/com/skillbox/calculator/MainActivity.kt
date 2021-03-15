8a39ae7434b226e4c600f7af1f18376c9d61e25d
DeveloperSasha1 <aleksandrshekin70@gmail.com>
2021-03-15 12:53:18 +0300

    Start

---
+++ app/src/main/java/com/skillbox/calculator/MainActivity.kt
@@ -0,0 +1,85 @@
+package com.skillbox.calculator
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.util.Log
+import kotlinx.android.synthetic.main.activity_main.*
+import net.objecthunter.exp4j.ExpressionBuilder
+import java.lang.Exception
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+
+        bracket_open_btn.setOnClickListener{setTextFields("(")}
+        bracket_close_btn.setOnClickListener{setTextFields(")")}
+        division_btn.setOnClickListener{setTextFields("/")}
+
+        btn_7.setOnClickListener{setTextFields("7")}
+        btn_8.setOnClickListener{setTextFields("8")}
+        btn_9.setOnClickListener{setTextFields("9")}
+        mult_btn.setOnClickListener{setTextFields("*")}
+
+        btn_4.setOnClickListener{setTextFields("4")}
+        btn_5.setOnClickListener{setTextFields("5")}
+        btn_6.setOnClickListener{setTextFields("6")}
+        plus_btn.setOnClickListener{setTextFields("+")}
+
+        btn_1.setOnClickListener{setTextFields("1")}
+        btn_2.setOnClickListener{setTextFields("2")}
+        btn_3.setOnClickListener{setTextFields("3")}
+        minus_btn.setOnClickListener{setTextFields("-")}
+
+        btn_dot.setOnClickListener{setTextFields(".")}
+        btn_0.setOnClickListener{setTextFields("0")}
+        equals_btn.setOnClickListener{setTextFields("=")}
+
+        clear_btn.setOnClickListener{
+            math_operation.text = ""
+            result_text.text = ""
+
+        }
+
+
+
+        btn_back.setOnClickListener {
+            var str = math_operation.text.toString()
+            if (str.isNotEmpty())
+                math_operation.text = str.substring(0, str.length - 1)
+            result_text.text = ""
+
+        }
+
+        equals_btn.setOnClickListener {
+            try {
+                val ex = ExpressionBuilder(math_operation.text.toString()).build()
+                val result = ex.evaluate()
+
+                val longRes = result.toLong()
+                if (result == longRes.toDouble())
+                    result_text.text = longRes.toString()
+                else
+                    result_text.text = result.toString()
+
+            }catch (e:Exception){
+                Log.d("Ошибкка", "сообщение: ${e.message}")
+
+            }
+        }
+
+
+
+    }
+    fun setTextFields(str:String){
+        if (result_text.text != ""){
+            math_operation.text =result_text.text
+            result_text.text = ""
+        }
+         math_operation.append(str)
+
+
+    }
+}
